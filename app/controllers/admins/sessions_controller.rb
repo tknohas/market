da683fb30649d23627b5f54658e91d301b8a904f
Masashi Takahashi <0xv17851726325v@gmail.com>
2025-01-05 15:49:47 +0900

    ユーザーがログインできるように

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -2,7 +2,7 @@ class Admins::SessionsController < Devise::SessionsController
   layout 'admins/application'
 
   def after_sign_in_path_for(_resource)
-    root_path
+    admins_products_path
   end
 
   def after_sign_out_path_for(_resource)
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
+end
